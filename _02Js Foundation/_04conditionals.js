59c421f331502b2f86b42b1ff0fa0bc6774d7b4f
arka <arkagarai292@gmail.com>
2026-02-06 01:57:19 +0530

    changes

--- _02Js Foundation/_04conditionals.js
+++ _02Js Foundation/_04conditionals.js
@@ -3,8 +3,8 @@ let num1 = 10;
 let num2 = 20;
 
 if (num1 > num2) {
-    console.log(num1 + " is greater than " + num2); 
-}else {
+    console.log(num1 + " is greater than " + num2);
+} else {
     console.log(num1 + " is not greater than " + num2);
 }
 
@@ -18,7 +18,7 @@ if (typeof score === "number") {
 }
 
 //check if a boolean value is true or false
-let isLoggedIn = true;  
+let isLoggedIn = true;
 
 if (isLoggedIn) {
     console.log("User is logged in.");
